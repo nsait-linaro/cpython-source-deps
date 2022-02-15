e9fa49eabc9ad75d46a6b98fad64cc8bbcc1b857
Niyas Sait <niyas.sait@linaro.org>
2022-02-15 07:52:17 +0000

    add build_nmakehlp.ps1

--- build.ps1
+++ build.ps1
@@ -1,5 +1,6 @@
 
-cmd.exe /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat`" && set > %temp%\vcvars32.txt"
+$vcvarsarch = "32"
+cmd.exe /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars$vcvarsarch.bat`" && set > %temp%\vcvars$vcvarsarch.txt"
 Get-Content "$env:temp\vcvars$vcvarsarch.txt" | Foreach-Object {
     if ($_ -match "^(.*?)=(.*)$") {
         Set-Content "env:\$($matches[1])" $matches[2]
--- build_nmakehlp.ps1
+++ build_nmakehlp.ps1
@@ -1,4 +1,9 @@
-cmd.exe /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat`" && set > %temp%\vcvars64.txt"
+<#
+#>
+
+$vcvarsarch="64"
+
+cmd.exe /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars$vcvarsarch.bat`" && set > %temp%\vcvars$vcvarsarch.txt"
 Get-Content "$env:temp\vcvars$vcvarsarch.txt" | Foreach-Object {
     if ($_ -match "^(.*?)=(.*)$") {
         Set-Content "env:\$($matches[1])" $matches[2]
