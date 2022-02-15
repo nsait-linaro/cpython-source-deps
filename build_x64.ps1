428f7b7b875b0255c08966c5f73bdab3ab8f7814
Niyas Sait <niyas.sait@linaro.org>
2022-02-15 19:10:36 +0000

    add config

--- build_x64.ps1
+++ build_x64.ps1
@@ -1,5 +1,5 @@
 
-$vcvarsarch = "32"
+$vcvarsarch = "64"
 cmd.exe /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars$vcvarsarch.bat`" && set > %temp%\vcvars$vcvarsarch.txt"
 Get-Content "$env:temp\vcvars$vcvarsarch.txt" | Foreach-Object {
     if ($_ -match "^(.*?)=(.*)$") {
